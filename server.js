0fe2f878bbfb99798f3d6dbe6bd80207cde06388
codeponics <143466421+codeponics@users.noreply.github.com>
2026-08-27 17:48:47 -0400

    fix: remove useless host addr

--- server.js
+++ server.js
@@ -20,7 +20,6 @@ wss.on('connection', (ws) => {
 });
 
 const PORT = process.env.PORT || 3000;
-const HOST = '0.0.0.0';
-server.listen(PORT, HOST, () => {
-  console.log(`Beat Battle listening on http://${HOST}:${PORT}`);
+server.listen(PORT, () => {
+  console.log(`Beat Battle listening on http://localhost:${PORT}`);
 });
