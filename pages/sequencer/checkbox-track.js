c607e7a5d69dbe51703d9325c3623ae5294c9671
Michael Lee <118201480+leezmc@users.noreply.github.com>
2026-08-30 21:53:28 -0400

    sound effect on add node

--- pages/sequencer/checkbox-track.js
+++ pages/sequencer/checkbox-track.js
@@ -1,10 +1,15 @@
-export function buildCheckboxTrack(trackEl, steps, prevChecked) {
+export function buildCheckboxTrack(trackEl, steps, prevChecked, onCheck) {
   while (trackEl.children.length > 1) trackEl.removeChild(trackEl.lastChild);
 
   return Array.from({ length: steps }, (_, step) => {
     const checkbox = document.createElement('input');
     checkbox.type = 'checkbox';
     checkbox.checked = !!prevChecked?.[step];
+    if (onCheck) {
+      checkbox.addEventListener('change', () => {
+        if (checkbox.checked) onCheck(step);
+      });
+    }
     trackEl.appendChild(checkbox);
     return checkbox;
   });
